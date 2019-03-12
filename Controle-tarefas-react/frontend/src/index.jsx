3cbcfb32e6915f83f6fb074dd2e3762b1660b0c4
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-03-12 20:42:19 -0300

    action creator of search and middleware React-Promise

--- Controle-tarefas-react/frontend/src/index.jsx
+++ Controle-tarefas-react/frontend/src/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './main/app'
-import {createStore} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
 import {Provider} from 'react-redux'
+import promise from 'redux-promise'
 
 import reducers from './main/reducers'
 
-const store = createStore(reducers)
+/* Baixar o plugin do Google Chrome: 
+ https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd
+*/
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+&& window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = applyMiddleware(promise)(createStore)(reducers, devTools)
 
 
 ReactDOM.render(
--- Controle-tarefas-react/frontend/src/todo/todoForm.jsx
+++ Controle-tarefas-react/frontend/src/todo/todoForm.jsx
@@ -1,44 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { changeDescription } from './todoActions'
+import { changeDescription, search } from './todoActions'
 
-const TodoForm = props => {
+class TodoForm extends Component {
+    constructor(props) {
+        super(props)
+        this.keyHandler = this.keyHandler.bind(this)
+    }
 
-    /*adiciona teclas de atalhos
-        shift+enter para pesquisa
-        enter para adição de todos
-        esc para limpar formulário
-    */
-    const keyHandler = (e) => {
+    // componentWillMount is deprecated
+    componentDidMount(){
+        this.props.search()
+    }
+
+    keyHandler(e) {
         if (e.key === 'Enter') {
-            e.shiftKey ? props.handleSearch() : props.handleAdd()
+            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
         } else if (e.key === 'Escape') {
             props.handleClear()
         }
     }
 
-    return (
-        <div role='form' className='todoForm'>
-            <Grid cols='12 9 10'>
-                <input id='description' className='form-control' placeholder='Adicione uma tarefa'
-                    value={props.description} onChange={props.changeDescription}
-                    onKeyUp={keyHandler} />
-            </Grid>
-            <Grid cols='12 3 2'>
-                <IconButton style='primary' icon='plus' onClick={props.handleAdd}>
-                </IconButton>
-                <IconButton style='info' icon='search' onClick={props.handleSearch} />
-                <IconButton style='default' icon='close' onClick={props.handleClear} />
-            </Grid>
-        </div>
-    )
+    render() {
+        return (
+            <div role='form' className='todoForm'>
+                <Grid cols='12 9 10'>
+                    <input id='description' className='form-control' placeholder='Adicione uma tarefa'
+                        value={this.props.description} onChange={this.props.changeDescription}
+                        onKeyUp={this.keyHandler} />
+                </Grid>
+                <Grid cols='12 3 2'>
+                    <IconButton style='primary' icon='plus' onClick={this.props.handleAdd}>
+                    </IconButton>
+                    <IconButton style='info' icon='search' onClick={this.props.handleSearch} />
+                    <IconButton style='default' icon='close' onClick={this.props.handleClear} />
+                </Grid>
+            </div>
+        )
+    }
+
 }
 
 const mapStateToProps = state => ({ description: state.todo.description })
-const mapDispatchToProps = dispatch => 
-    bindActionCreators({changeDescription}, dispatch)
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ changeDescription, search }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
