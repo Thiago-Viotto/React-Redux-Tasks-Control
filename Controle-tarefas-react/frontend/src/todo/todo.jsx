310ba96e6acdbc19fa134c4025e3ae281a5e6ae1
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2019-03-20 19:49:21 -0300

    add state in action search

--- Controle-tarefas-react/frontend/src/todo/todo.jsx
+++ Controle-tarefas-react/frontend/src/todo/todo.jsx
@@ -7,10 +7,7 @@ import TodoList from '../todo/todoList'
 export default props => (
     <div>
         <PageHeader name='Tarefas' small='Cadastro' />
-        <TodoForm description={this.state.description} handleAdd={this.handleAdd}
-            handleChange={this.handleChange} handleSearch={this.handleSearch}
-            handleClear={this.handleClear} />
-        <TodoList list={this.state.list} handleRemove={this.handleRemove}
-            handleMarkAsPending={this.handleMarkAsPending} handleMarkAsDone={this.handleMarkAsDone} />
+        <TodoForm />
+        <TodoList />
     </div>
 )
